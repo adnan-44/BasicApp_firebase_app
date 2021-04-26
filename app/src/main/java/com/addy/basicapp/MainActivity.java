f5289af5024c936e2c24be374bc7383a750fcc53
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-27 02:21:38 +0530

    app: create a new user with email and password
    * Get refrence to sign_up button and set onclick listener to it.
    * If user get successfully created or failed, show toasts accordingly.

--- app/src/main/java/com/addy/basicapp/MainActivity.java
+++ app/src/main/java/com/addy/basicapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.addy.basicapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText email_input, password_input;
+    private Button sign_up;
+
+    // Firebase Authentication instance
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +31,30 @@ public class MainActivity extends AppCompatActivity {
         // init vars
         email_input = findViewById(R.id.email_input);
         password_input = findViewById(R.id.password_input);
+        sign_up = findViewById(R.id.sign_up);
+
+        // get a FirebaseAuth instance to work with firebase
+        auth = FirebaseAuth.getInstance();
+
+        // Set sign-up button to create a user for firebase
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create a new user with email and password
+                auth.createUserWithEmailAndPassword(email_input.getText().toString(), password_input.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // Check whether task is successful or not
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Signed-up Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Failed to sign-up", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
