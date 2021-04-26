d59b6942957727eca8a6648cc92dcc688c7a341f
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-27 00:47:47 +0530

    app: layout: added EditText and button
    EditText for taking email and password input and button for action, and a TextView for login
    also Add there refrence in Activity.

--- app/src/main/java/com/addy/basicapp/MainActivity.java
+++ app/src/main/java/com/addy/basicapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.addy.basicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email_input, password_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // init vars
+        email_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.password_input);
     }
 }
