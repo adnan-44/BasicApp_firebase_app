a1ab916676050c5a5382e8fe2a96364fe60ad421
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-27 01:40:07 +0530

    app: activity: added new SuccessfulSignActivity
    For successful sign-in. Added a TextView(to display successful message) and a button (Will add functionality to open login)

---
+++ app/src/main/java/com/addy/basicapp/SuccessfulSignActivity.java
@@ -0,0 +1,14 @@
+package com.addy.basicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessfulSignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successfull_sign);
+    }
+}
