89d95820d1ca8cfa356f7e9073d6bff6fb0ca967
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-29 16:25:34 +0530

    app: Added EditText and Button in activity_login
    * Add there refrence in LoginActivity class

--- app/src/main/java/com/addy/basicapp/LoginActivity.java
+++ app/src/main/java/com/addy/basicapp/LoginActivity.java
@@ -3,12 +3,19 @@ package com.addy.basicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email_input, password_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Init vars
+        email_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.password_input);
     }
 }
