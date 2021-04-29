760491b3688f985720edf26c4da3fa368a337e17
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-30 04:05:11 +0530

    app: Added basic GUI elements to create_profile layout
    * Added a ImageView (to select and upload profile image to it), two EditTexts for full_name input and phone number, and a button to create profile.
    * Added id to these created views, and add reference in Activity.
    * DashboardActivity: set onClickListener on create_profile button to open CreateProfileActivity using intents.

--- app/src/main/java/com/addy/basicapp/CreateProfileActivity.java
+++ app/src/main/java/com/addy/basicapp/CreateProfileActivity.java
@@ -2,12 +2,25 @@ package com.addy.basicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class CreateProfileActivity extends AppCompatActivity {
+
+    private EditText full_name_input, phone_input;
+    private Button create_profile;
+    private ImageView profile_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+        // Init vars reference
+        full_name_input = findViewById(R.id.full_name_input);
+        phone_input = findViewById(R.id.phone_input);
+        create_profile = findViewById(R.id.create_profile);
+        profile_image = findViewById(R.id.profile_image);
     }
 }
--- app/src/main/java/com/addy/basicapp/DashboardActivity.java
+++ app/src/main/java/com/addy/basicapp/DashboardActivity.java
@@ -2,7 +2,9 @@ package com.addy.basicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
@@ -17,5 +19,14 @@ public class DashboardActivity extends AppCompatActivity {
         // Init vars
         my_profile = findViewById(R.id.my_profile);
         create_profile = findViewById(R.id.create_profile);
+
+        // Open CreateProfileActivity onclick of create_profile button
+        create_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, CreateProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
