4bac4cbc9df691e3ad8847c8d43bc2d84758aa9b
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-30 03:56:27 +0530

    app: Added blank CreateProfileActivity
    This Activity will be used to create profile, like Full name, phone number, profile picture etc.

---
+++ app/src/main/java/com/addy/basicapp/CreateProfileActivity.java
@@ -0,0 +1,13 @@
+package com.addy.basicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+}
