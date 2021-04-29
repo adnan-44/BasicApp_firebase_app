f308d65dc38ebf4f55a2cfb91403570bce9fc0a3
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-29 16:49:38 +0530

    app: Login user with firebase auth
    * Get refrence to login button.
    * Get instance of FirebaseAuth.
    * Set onClickLietener to login a user with FirebaseAuth using EditTexts(email and password)
    * And show Toasts as of now.

--- app/src/main/java/com/addy/basicapp/LoginActivity.java
+++ app/src/main/java/com/addy/basicapp/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.addy.basicapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email_input, password_input;
+    private Button login;
+
+    // Firebase Authentication instance
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +31,32 @@ public class LoginActivity extends AppCompatActivity {
         // Init vars
         email_input = findViewById(R.id.email_input);
         password_input = findViewById(R.id.password_input);
+        login = findViewById(R.id.login);
+
+        // get a FirebaseAuth instance to work with firebase
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    // Login user with email and password
+                    auth.signInWithEmailAndPassword(email_input.getText().toString(), password_input.getText().toString())
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(LoginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        Toast.makeText(LoginActivity.this, "Failed to login", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }catch(IllegalArgumentException e){
+                    Toast.makeText(LoginActivity.this, "Input fields can't be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
