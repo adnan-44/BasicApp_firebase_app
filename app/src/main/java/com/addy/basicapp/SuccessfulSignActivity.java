30892224a54bfceeda31b8454aa85a9889417daf
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-29 19:36:38 +0530

    app: open LoginActivity onclick of login button
    * in SuccessfulSignActivity
    * set login id to login button xml

--- app/src/main/java/com/addy/basicapp/SuccessfulSignActivity.java
+++ app/src/main/java/com/addy/basicapp/SuccessfulSignActivity.java
@@ -2,13 +2,30 @@ package com.addy.basicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccessfulSignActivity extends AppCompatActivity {
+    
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successful_sign);
+
+        // Init vars
+        login_button = findViewById(R.id.login);
+
+        // Set onclick listener for button to open LoginActivity
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SuccessfulSignActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
