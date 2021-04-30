187f8825d51a19ea1a92542adbd463a23587512f
adnan-44 <mohd.adnan0405@gmail.com>
2021-05-01 05:13:42 +0530

    app: Added TextView and ImageView to show user_info
    in my_profile layout
    * Added CardView to look better, set their reference in Activity file.
    * Made DashboardAcitivity as parent to this (to get back icon on action bar)

--- app/src/main/java/com/addy/basicapp/MyProfileActivity.java
+++ app/src/main/java/com/addy/basicapp/MyProfileActivity.java
@@ -3,12 +3,24 @@ package com.addy.basicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+    // GUI elements
+    private TextView full_name_text, phone_text, email_text;
+    private ImageView profile_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        // Init vars
+        full_name_text = findViewById(R.id.full_name_text);
+        phone_text = findViewById(R.id.phone_text);
+        email_text = findViewById(R.id.email_text);
+        profile_image = findViewById(R.id.profile_image);
     }
 }
