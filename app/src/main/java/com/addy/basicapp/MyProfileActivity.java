3752e68dd56a3a5f682abd7c1848554e0f7fed14
adnan-44 <mohd.adnan0405@gmail.com>
2021-05-01 05:39:22 +0530

    app: get and set profile image from Firebase Storage
    * using DownloadUrl() and Glide to do download and set Image to Profile ImageView automatically.
    * To use Glide, we need to add FirebaseUI dependency.
    * gradle: added 'com.firebaseui:firebase-ui-storage:6.4.0'

--- app/src/main/java/com/addy/basicapp/MyProfileActivity.java
+++ app/src/main/java/com/addy/basicapp/MyProfileActivity.java
@@ -1,18 +1,26 @@
 package com.addy.basicapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MyProfileActivity extends AppCompatActivity {
 
@@ -23,6 +31,7 @@ public class MyProfileActivity extends AppCompatActivity {
     //Firebase for authentication and retrieve data from FireStore
     private FirebaseAuth auth;
     private FirebaseFirestore firebaseFirestore;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,7 @@ public class MyProfileActivity extends AppCompatActivity {
         // Firebase reference to work with it
         auth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         // Get document of Uid name, because we saved like this before
         DocumentReference documentReference = firebaseFirestore.collection("user_info").document(auth.getUid());
@@ -48,6 +58,22 @@ public class MyProfileActivity extends AppCompatActivity {
                 full_name_text.setText(value.getString("full_name"));
                 phone_text.setText(value.getString("phone"));
                 email_text.setText(auth.getCurrentUser().getEmail());       // Get current user email from auth reference
+
+                // Get DownloadUrl method to get URL and then Use Glide to download and set that in ImageView automatically
+                StorageReference imageRef = storageReference.child(auth.getUid()+"/profile");       // Because we've stored profile image in Uid/profile
+                imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // FirebaseUI dependency using Glide made easier to do automatically
+                        Glide.with(MyProfileActivity.this).load(uri).into(profile_image);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Show failed toast if something gets wrong
+                        Toast.makeText(MyProfileActivity.this, "Failed to download profile",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
